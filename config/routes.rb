606a58315f8220b5c50112447bfd34b837deee24
ohtubo <brte14020@gmail.com>
2021-08-13 22:45:29 +0000

    [Add]_relationships_create_destroy_index

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   
-  resources :users, only: [:show,:edit]
+  resources :users, only: [:show, :edit, :update] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
