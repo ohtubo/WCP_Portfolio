e520cbb456ab7d8198920cc833e5bd61e755b158
ohtubo <brte14020@gmail.com>
2021-08-25 08:54:26 +0000

    [Add]_layouts_[top_searches_usersProfile]

--- app/controllers/scenario_favorites_controller.rb
+++ app/controllers/scenario_favorites_controller.rb
@@ -3,14 +3,14 @@ class ScenarioFavoritesController < ApplicationController
     scenario = Scenario.find(params[:scenario_id])
     favorite = current_user.scenario_favorites.new(scenario_id: scenario.id)
     favorite.save
-    redirect_to scenario_path(scenario)
+    redirect_to request.referer
   end
 
   def destroy
     scenario = Scenario.find(params[:scenario_id])
     favorite = current_user.scenario_favorites.find_by(scenario_id: scenario.id)
     favorite.destroy
-    redirect_to scenario_path(scenario)
+    redirect_to request.referer
   end
 
   # def show
