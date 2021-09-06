d391158355f8a92b123ccc371af53fcf7c3e451b
ohtubo <brte14020@gmail.com>
2021-09-06 14:41:26 +0000

    [Add]_comment-models-scenario_comment scenario_favorite

--- app/models/scenario_favorite.rb
+++ app/models/scenario_favorite.rb
@@ -1,4 +1,6 @@
 class ScenarioFavorite < ApplicationRecord
+#アソシエーションの設定-------------------------------------------------
   belongs_to :user
   belongs_to :scenario
+#-----------------------------------------------------------------------
 end
